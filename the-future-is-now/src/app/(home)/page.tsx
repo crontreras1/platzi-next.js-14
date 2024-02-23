a4193eb0cb840b6c71b921534dbd0a6a6183b58e
crontreras1 <crontreras1@gmail.com>
2024-02-23 15:12:08 -0500

    class 38. SEO

--- the-future-is-now/src/app/(home)/page.tsx
+++ the-future-is-now/src/app/(home)/page.tsx
@@ -1,4 +1,10 @@
 import { MainProducts } from 'app/components/home/MainProducts'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'future world',
+  description: 'wellcome to the future world, and e-commerce from the other century'
+}
 
 export default function Home() {
   return (
