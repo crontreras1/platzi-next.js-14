a6d884a3ca7510a8107091cf56704ff96c6369f4
crontreras1 <crontreras1@gmail.com>
2024-03-31 19:04:48 -0500

    class 51. implement a sales bot in the app

--- the-future-is-now/src/app/chat/page.tsx
+++ the-future-is-now/src/app/chat/page.tsx
@@ -1,11 +1,18 @@
 import { Chat } from "app/components/chat/Chat"
+import { getProducts } from "app/services/shopify/products"
+import { createAgent } from "app/utils/openai/createAgent"
 
-export default function chatPage () {
+export default async function chatPage () {
+    const products = await getProducts()
+    const productTitle = products.map((item: any) => item.title )
+    const flatProductTitle = productTitle.join('\n')
+    const agent = createAgent(flatProductTitle)
+
     return (
         <>
             <h1>Chatbot</h1>
             
-            <Chat />
+            <Chat agent={ agent }/>
         </>
     )
 }
